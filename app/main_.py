2ba7e6885344999917e7f01f3d6da35329e8584d
WindGiang <572936935@qq.com>
2020-03-11 15:26:05 +0800

    主要文件，设置了环境变量获取密钥

--- app/main_.py
+++ app/main_.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, flash
+import os
+
+from flask import Flask, render_template, flash, make_response, send_file, redirect, session, url_for
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, PasswordField
 from wtforms.validators import DataRequired
@@ -6,8 +8,7 @@ from APIToJSON import JsonCreator
 from JsonToCal import ical_creat
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'windgiang'
-
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'a hard string you never guess'
 
 class LoginForm(FlaskForm):
     user = StringField(
@@ -29,20 +30,25 @@ def main_app():
     form = LoginForm()
     if form.validate_on_submit():
         user = form.user.data
+        session['user'] = user
         password = form.password.data
-        coursejson = JsonCreator(user, password)
-        if coursejson.stu.isLogin:
-            coursejson.creatJson()
-            flash('json解析成功，正在导出ICS文件，请等待')
-            response = ical_creat(user)
+        jsoncreator = JsonCreator(user, password)
+        if jsoncreator.stu.isLogin:
+            jsoncreator.creatJson()
+            ical_creat(user)
             flash('ics文件生成成功')
-            return response
+            return redirect(url_for('downloadicsfile'))
         else:
             flash('帐号密码有误，请重试')
+            form.user.data = session.get('user')
+            return render_template('index.html', form=form)
     return render_template('index.html', form=form)
 
 
-@app.route('/downloadicsfile/<filename>', methods=['GET', 'POST'])
-def ics_file(filename):
-    return 'hello ,'+filename+' no exist'
+@app.route('/downloadicsfile', methods=['GET', 'POST'])
+def downloadicsfile():
+    filename = session.get('user')
+    response = make_response(send_file(os.path.abspath('cache/ics/' + filename + '.ics')))
+    response.headers["Content-Disposition"] = "attachment; filename=CourseTable-2019-2020-2.ics;"
+    return response
 
